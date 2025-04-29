5cfed4b1aa6bd179ce4d9a4d6f496a9c0c23f7ec
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-29 18:20:50 +0100

    Update Fonts.js

---
+++ src/constants/Fonts.js
@@ -0,0 +1,18 @@
+// constants/fonts.js
+
+const Fonts = {
+    // Primary Fonts
+    primaryRegular: 'Roboto-Regular',
+    primaryBold: 'Roboto-Bold',
+  
+    // Secondary Fonts
+    secondaryRegular: 'OpenSans-Regular',
+    secondaryBold: 'OpenSans-Bold',
+  
+    // Additional Fonts
+    heading: 'Lora-Bold',
+    body: 'Lora-Regular',
+  };
+  
+  export default Fonts;
+  
