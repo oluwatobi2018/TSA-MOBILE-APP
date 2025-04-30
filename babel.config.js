4caec1305977c8a298e689c3a0686e0134a1205b
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-30 09:55:42 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,9 @@
+module.exports = {
+  presets: [
+    'module:metro-react-native-babel-preset'
+  ],
+  plugins: [
+    'react-native-reanimated/plugin' // If you're using React Native Reanimated
+  ]
+};
 
