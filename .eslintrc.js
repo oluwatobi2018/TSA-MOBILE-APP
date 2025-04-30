b1627cfdda9e0e1d7d8c65378e36b93727a92f0b
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-30 09:50:19 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,17 @@ module.exports = {
     '@react-native',
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   env: {
     browser: true,
     node: true,
     es6: true,
   },
   rules: {
-    // Customize your rules
-    'react/react-in-jsx-scope': 'off', // not needed in React 17+
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
   settings: {
     react: {
@@ -28,4 +22,3 @@ module.exports = {
     },
   },
 };
-
