129291aa03ddce80ad40fc4624da8345258a2d5f
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-30 20:12:54 +0100

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,8 +1,12 @@
+// AppNavigator.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import DetailsScreen from '../screens/DetailsScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import SocialScreen from '../screens/SocialScreen';
+import LinksScreen from '../screens/LinksScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +16,9 @@ const AppNavigator = () => {
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="Profile" component={ProfileScreen} />
+                <Stack.Screen name="Social" component={SocialScreen} />
+                <Stack.Screen name="Links" component={LinksScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
