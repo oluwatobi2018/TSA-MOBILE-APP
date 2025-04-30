12545f44d183f1d1d858170e1071c4b4a6505d93
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-30 09:58:43 +0100

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,1 +1,20 @@
+// metro.config.js
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = {
+  transformer: {
+    // Enable support for TypeScript or JSX transformation
+    babelTransformerPath: require.resolve('react-native-typescript-transformer')
+  },
+  resolver: {
+    // Customize module resolution settings (optional)
+    blacklistRE: /#.*$/, // Example to blacklist certain files, such as test files
+    extraNodeModules: {
+      // Example to include additional libraries or change paths for modules
+    }
+  },
+  watchFolders: [
+    // Specify additional folders to watch for file changes, if necessary
+  ]
+};
 
