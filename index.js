e94623f03c7ec334fcba127aa752e0efd78a7c90
Oluwatobi Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2025-04-30 09:57:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App'; // Import your main app component
+import { name as appName } from './app.json'; // The app name from app.json
+
+// Register the main app component with the React Native app registry
+AppRegistry.registerComponent(appName, () => App);
 
